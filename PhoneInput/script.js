23d3e51b67411f6409e832b802565147ec6610f0
IslemMedjahdi <ki_medjahdi@esi.dz>
2022-11-30 13:34:29 +0100

    adding phone input

--- PhoneInput/script.js
+++ PhoneInput/script.js
@@ -3,12 +3,22 @@ const dropdown = document.getElementById("dropdown");
 const input = document.getElementById("input");
 let error = document.getElementById("error");
 
+const masks = {
+  algeria: "+213 ___ __ __ __",
+  unitedStates: "+1 (___) ___-____",
+};
+
 // states
 let dropDownOpen = false;
 
 let currentState = "algeria";
 
 let inputState = "";
+
+const setInputState = (inputStateP) => {
+  input.setAttribute("value", inputState);
+  inputState = inputStateP;
+};
 
 // handlers
 const closeDropDown = () => {
@@ -27,111 +37,21 @@ const toggleDropDown = () => {
   dropDownOpen = !dropDownOpen;
 };
 
-const formatNumber = (
-  phoneNumberString = inputState,
-  currentStateVar = currentState
-) => {
-  error.innerHTML = "";
-
-  // clean phone number
-  phoneNumberString = (phoneNumberString + "")
-    .replaceAll("(", "")
-    .replaceAll(")", "")
-    .replaceAll("+", "")
-    .replaceAll("-", "")
-    .replaceAll(" ", "");
-  console.log(phoneNumberString);
-  switch (currentStateVar) {
-    case "unitedStates": {
-      console.log("rani khadam");
-      const cleaned = ("" + phoneNumberString).replace(/\D/g, "");
-      const match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
-      if (match) {
-        var intlCode = match[1] ? "+1 " : "";
-        return [intlCode, "(", match[2], ") ", match[3], "-", match[4]].join(
-          ""
-        );
-      }
-      break;
-    }
-    case "germany": {
-      if (
-        phoneNumberString.length === 14 &&
-        phoneNumberString.startsWith("49")
-      ) {
-        return [
-          "+",
-          phoneNumberString.substring(0, 2),
-          " ",
-          phoneNumberString.substring(2, 4),
-          " ",
-          phoneNumberString.substring(4),
-        ].join("");
-      }
-      break;
-    }
-    case "algeria": {
-      if (
-        phoneNumberString.length === 12 &&
-        phoneNumberString.startsWith("213")
-      ) {
-        return [
-          "+",
-          "213",
-          " ",
-          phoneNumberString.substring(3, 6),
-          " ",
-          phoneNumberString.substring(6).match(/.{1,2}/g)
-            ? phoneNumberString
-                .substring(6)
-                .match(/.{1,2}/g)
-                .join("")
-            : "",
-        ].join("");
-      }
-      break;
-    }
-    case "uruguay": {
-      if (
-        phoneNumberString.startsWith(598) &&
-        phoneNumberString.length === 15
-      ) {
-        console.log(phoneNumberString.substring(2));
-        return [
-          "598",
-          " ",
-          phoneNumberString.substring(3).match(/.{1,3}/g)
-            ? phoneNumberString
-                .substring(3)
-                .match(/.{1,3}/g)
-                .join(" ")
-            : "",
-        ].join("");
-      }
-      break;
-    }
-    case "china": {
-      if (phoneNumberString.length === 13 && phoneNumberString.startsWith(86)) {
-        return ["+86", " ", phoneNumberString.substring(2)].join("");
-      }
-      break;
-    }
-    default:
-      return phoneNumberString;
-  }
-  if (phoneNumberString) {
-    error.innerHTML = "error";
-  }
-  return phoneNumberString;
+const allowInput = (inputId) => {
+  console.log(inputId);
+  document.querySelectorAll(".input").forEach((item) => {
+    if (item.id !== inputId) item.style.display = "none";
+  });
+  document.getElementById(inputId).style.display = "block";
 };
 
 // events
 for (let i = 0; i < dropdown.children.length; i++) {
   const child = dropdown.children[i];
   child.addEventListener("click", () => {
-    input.value = formatNumber(undefined, child.id);
     currentState = child.id;
     flagChooser.innerHTML = `<img src="resources/${child.id}.svg" class="flag" />`;
+    allowInput(child.id + "-input");
     closeDropDown();
   });
 }
@@ -140,7 +60,38 @@ flagChooser.addEventListener("click", () => {
   toggleDropDown();
 });
 
-input.addEventListener("input", (e) => {
-  inputState = e.target.value;
-  input.value = formatNumber(e.target.value);
+// This code empowers all input tags having a placeholder and data-slots attribute
+document.addEventListener("DOMContentLoaded", () => {
+  for (const el of document.querySelectorAll("[placeholder][data-slots]")) {
+    const pattern = el.getAttribute("placeholder"),
+      slots = new Set(el.dataset.slots || "_"),
+      prev = ((j) =>
+        Array.from(pattern, (c, i) => (slots.has(c) ? (j = i + 1) : j)))(0),
+      first = [...pattern].findIndex((c) => slots.has(c)),
+      accept = new RegExp(el.dataset.accept || "\\d", "g"),
+      clean = (input) => {
+        input = input.match(accept) || [];
+        return Array.from(pattern, (c) =>
+          input[0] === c || slots.has(c) ? input.shift() || c : c
+        );
+      },
+      format = () => {
+        const [i, j] = [el.selectionStart, el.selectionEnd].map((i) => {
+          i = clean(el.value.slice(0, i)).findIndex((c) => slots.has(c));
+          return i < 0
+            ? prev[prev.length - 1]
+            : back
+            ? prev[i - 1] || first
+            : i;
+        });
+        el.value = clean(el.value).join``;
+        el.setSelectionRange(i, j);
+        back = false;
+      };
+    let back = false;
+    el.addEventListener("keydown", (e) => (back = e.key === "Backspace"));
+    el.addEventListener("input", format);
+    el.addEventListener("focus", format);
+    el.addEventListener("blur", () => el.value === pattern && (el.value = ""));
+  }
 });
---
+++ Swiper Animation/solution/script.js
@@ -0,0 +1,3 @@
+const card1 = document.getElementById("card-1");
+const card2 = document.getElementById("card-2");
+const card3 = document.getElementById("card-3");
