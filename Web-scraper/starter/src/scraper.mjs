af3f60c3403b233ec9c745bbb69f449e385c9edc
IslemMedjahdi <ki_medjahdi@esi.dz>
2022-11-30 19:26:13 +0100

    adding scrapper

--- Web-scraper/starter/src/scraper.mjs
+++ Web-scraper/starter/src/scraper.mjs
@@ -1,21 +1,31 @@
 import fetch from "node-fetch";
 import fs from "fs";
 import path from "path";
+import puppeteer from "puppeteer";
+
+const proxy = "http://my.proxy.com:3001";
+const username = "gdg_algiers";
+const password = "devfest2022";
+
 (async () => {
-  const response = await fetch(
-    "https://devfest22-recipes-heaven.gdgalgiers.com/api/recipes",
-    {
-      headers: {
-        authorization: "Basic Z2RnX2FsZ2llcnM6ZGV2ZmVzdDIwMjI=",
-      },
-    }
+  let launchOptions = { headless: false, args: ["--start-maximized"] };
+  const browser = await puppeteer.launch(launchOptions);
+  const page = await browser.newPage();
+  await page.authenticate({ username: username, password: password });
+  await page.goto("https://devfest22-recipes-heaven.gdgalgiers.com");
+  await page.goto(
+    "https://devfest22-recipes-heaven.gdgalgiers.com/api/recipes"
   );
-  const data = await response.json();
+  await page.content();
+  const innerText = await page.evaluate(() => {
+    return JSON.parse(document.querySelector("body").innerText);
+  });
+  console.log(innerText);
   await fs.promises.writeFile(
     path.join(".", "recepies.json"),
-    JSON.stringify({ recipes: data }),
-    "utf8"
+    JSON.stringify({ recepies: innerText }),
+    "utf-8"
   );
-  console.log(data);
+  await browser.close();
   console.log("I'm scraping information for you lazy stackoverflow-er ...");
 })();
